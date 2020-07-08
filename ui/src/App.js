a2af248875e5aaa113519302e1134aeef3eec714
ssmith <shawn.t.smith.777@gmail.com>
2020-07-08 11:11:44 -0500

    Basic Framework for 3 images/ docker stack

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
+const apiUrl = `http://localhost:8080`;
+
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await axios.get(apiUrl + '/users');
+    setUsers(res.data);
+    console.log(res.data)
+  };
+  const createUser = async () => {
+    await axios.get(apiUrl + '/user-create');
+    fetchData();
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => createUser()}>Create User</button>
+        <p>Users list:</p>
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>id: {user._id}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
